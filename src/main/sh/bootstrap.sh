167e26f5ea2c7e1f54b7a7a1a7964e38393199b8
SeanieGleason <seanie@gleason.tech>
2025-11-14 15:16:52 -0600

    Add pihole.sh saveFileConverter.sh

--- src/main/sh/bootstrap.sh
+++ src/main/sh/bootstrap.sh
@@ -1,6 +1,8 @@
 TEMP_DIR=$(mktemp -d) && \
 cd $TEMP_DIR && \
 yes | sudo apt-get install git && \
+git config --global user.email "seanie@gleason.tech" && \
+git config --global user.name "SeanieGleason"  && \
 sudo ssh-keyscan github.com | sudo tee -a /home/$USER/.ssh/known_hosts > /dev/null && \
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -q && \
 cat ~/.ssh/id_ed25519.pub && \
