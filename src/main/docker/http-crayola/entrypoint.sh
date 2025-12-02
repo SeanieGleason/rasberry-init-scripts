50ab3fa67372e5c51f4e2ab366164e3956c15e28
SeanieGleason <seanie@gleason.tech>
2025-12-02 15:16:52 -0600

    Add crayola.com

--- src/main/docker/http-crayola/entrypoint.sh
+++ src/main/docker/http-crayola/entrypoint.sh
@@ -33,8 +33,9 @@ if [ -z "$(ls -A /data)" ]; then
   echo "==> Download complete."
   echo "==> Moving to nginx index.html dir."
 
+  sudo mkdir /usr/share/nginx/html
   ls "./data/web.archive.org/web/${timestamp}id_/$file_url/"
-  mv "./data/web.archive.org/web/${timestamp}id_/$file_url/*" "./data"
+  mv "./data/web.archive.org/web/${timestamp}id_/$file_url/" "/usr/share/nginx/html"
   echo "==> Done move."
 else
   echo "==> Existing download detected. Skipping wget."
